f9322340c9ac5ee5be185709fcb7c01e2de02ff8
muzza74 <muzza74@gmail.com>
2014-07-03 02:15:18 +1000

    i am commit

---
+++ Game/Assets/Scripts/Motor.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum theAxes{x,y};
+
+public class Motor : MonoBehaviour {
+	public Vector3 velocity;
+	public float deceleration = 0.04f;
+	private float xTarget;
+	private float yTarget;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		updateVelocity();
+		transform.Translate(velocity);
+	}
+
+	public void setVelocity(Vector3 newVelocity)
+	{
+		velocity = newVelocity;
+	}
+
+	// updates the current vector target for velocity
+	public void updateAxis(theAxes axis, float value)
+	{
+		if (axis == theAxes.x)
+		{
+			xTarget = value;
+		}
+		if(axis == theAxes.y)
+		{
+			yTarget = value;
+		}
+	}
+
+	void updateVelocity ()
+	{
+		velocity.x = xTarget;
+		velocity.y = yTarget;
+		if(xTarget > 0)
+		{
+			if((xTarget - deceleration) < 0)
+			{
+				xTarget = 0;
+			} else {
+				xTarget -= deceleration;
+			}
+		} else if (xTarget < 0)
+		{
+			if((xTarget + deceleration) > 0)
+			{
+				xTarget = 0;
+			} else {
+				xTarget += deceleration;
+			}
+		}
+		if(yTarget > 0)
+		{
+			if((yTarget - deceleration) < 0)
+			{
+				yTarget = 0;
+			} else {
+				yTarget -= deceleration;
+			}
+		} else if (yTarget < 0)
+		{
+			if((yTarget + deceleration) > 0)
+			{
+				yTarget = 0;
+			} else {
+				yTarget += deceleration;
+			}
+		}
+	}
+}
--- Game/Assets/Scripts/Movement.cs
+++ Game/Assets/Scripts/Movement.cs
@@ -4,20 +4,28 @@ using System.Collections;
 public class Movement : MonoBehaviour {
 	public float speed = 0.5f;
 	public GameObject projectile;
+	private Vector3 inputVector;
+	private Motor theMotor;
 	// Use this for initialization
 	void Start () {
-
+		theMotor = gameObject.GetComponent<Motor>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetAxis("Vertical") != 0f) 
+		float vert = Input.GetAxis("Vertical");
+		float hori = Input.GetAxis("Horizontal");
+		if (vert != 0f) 
 		{
-			gameObject.transform.Translate(0,speed * Input.GetAxis("Vertical"),0);
+			theMotor.updateAxis(theAxes.y, (speed * vert));
+		} else {
+			inputVector.y = 0f;
 		}
-		if (Input.GetAxis("Horizontal") != 0f)
+		if (hori != 0f)
 		{
-			gameObject.transform.Translate(speed * Input.GetAxis("Horizontal"),0,0);
+			theMotor.updateAxis(theAxes.x, (speed * hori));
+		} else {
+			inputVector.x = 0f;
 		}
 		if (Input.GetButtonDown("Fire1"))
 		{
